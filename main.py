d9aff837baf7d33adc271ba1bbd6df93f84f3191
Marcin Bober <marcin@bober.pl>
2023-03-20 22:34:27 +0100

    Sketch of data preparation.
    See: #5

--- main.py
+++ main.py
@@ -38,12 +38,36 @@ def get_orders(after: dt.datetime, before: dt.datetime = dt.datetime.today()) ->
     return total_response
 
 
+response = get_orders(after=dt.datetime.today() - dt.timedelta(days=79))
 
+df = pd.DataFrame(response, columns=['id', 'total', 'status', 'line_items', 'customer_id',  'date_completed'])
+df['total'] =  pd.to_numeric(df['total'])
 
-response = get_orders(after=dt.datetime.today() - dt.timedelta(days=40))
+#
+df = df.set_index(['id'])
 
-pprint(len(response))
+## filter only completed
+df = df.loc[df['status'] == 'completed']
 
+new_df = pd.DataFrame()
 
+for pos in df.iterrows():
+    row = pos[1]
+    
+    for line in pos[1]['line_items']:
+        
+        new_series = pd.DataFrame({'cena': line['price'],
+                                'status': row['status'],
+                                'użytkownik': row['customer_id'],
+                                'data':row['date_completed'],
+                                'produkt': line['parent_name'] if line['parent_name'] is not None else line['name'],
+                                'wariant': line['meta_data'][0].get('value') if line['meta_data'] != [] else None
+                                }, index=[line['id']])
 
+        new_df = pd.concat([new_df, new_series], axis=0)
 
+ 
+new_df['cena'] =  pd.to_numeric(new_df['cena'])
+print(new_df)
+print(len(new_df))
+print(new_df.sum(numeric_only=True))
